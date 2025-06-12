2a324259b3705de72d35614652632716ed20ff2f
Adrian Hernandez-Mendez <adrianhdezm@users.noreply.github.com>
2025-06-13 01:30:57 +0200

    refactor(provider): ♻️ Rename fetch token helper file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export type { SapAiCoreModelId, SapAiCoreProvider, SapAiCoreProviderSettings } from './sap-ai-core-provider';
 export { createSapAiCore, sapAiCore } from './sap-ai-core-provider';
-export type { TokenServiceConfig } from './fetch-with-token';
-export { createFetchWithToken } from './fetch-with-token';
+export type { TokenServiceConfig } from './fetch-with-token-provider';
+export { createFetchWithToken } from './fetch-with-token-provider';
